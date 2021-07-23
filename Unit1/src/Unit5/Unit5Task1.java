eba58614df0b68c26c726fa318998871b9c3afcb
DzmitryTarasevich <tarasevichd87@gmail.com>
2021-07-24 00:23:41 +0300

    fixUnit5HomeWork

--- Unit1/src/Unit5/Unit5Task1.java
+++ Unit1/src/Unit5/Unit5Task1.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 
 public class Unit5Task1 {
     public static void main(String[] args) {
-        Computer comp = new Computer();
-        comp.info();
-        comp.on();
-        comp.off();
+//        Computer comp = new Computer(2);
+//        comp.info();
+//        comp.on();
+//        comp.off();
     }
 }
 
@@ -23,36 +23,51 @@ class Computer {
         System.out.println("CPU - " + CPU + "; RAM - " + RAM + "; HDD - " + HDD);
     }
 
+    public Computer(int deathResource) {
+        this.deathResource = deathResource;
+    }
+
     Scanner sc = new Scanner(System.in);
     Random rnd = new Random();
 
     void on() {
-        int a = rnd.nextInt(2);
-        int b;
-        do {
-            System.out.println("Введите 1 или 0 для включения");
-            b = sc.nextInt();
-        } while (b >= 2 || b < 0);
+        if (deathResource > 0) {
+            int a = rnd.nextInt(2);
+            int b;
+            do {
+                System.out.println("Введите 1 или 0 для включения");
+                b = sc.nextInt();
+            } while (b >= 2 || b < 0);
 
-        if (a == b) {
-            System.out.println("комп включен");
+            if (a == b) {
+                System.out.println("комп включен");
+            } else {
+                System.out.println("критическая ошибка - железу конец");
+                deathResource = 0;
+            }
         } else {
-            System.out.println("критическая ошибка - железу конец");
+            System.out.println("Железо свое отработало(");
+            deathResource = 0;
         }
     }
 
 
     void off() {
-        int a = rnd.nextInt(2);
-        int b = sc.nextInt();
-        do {
-            System.out.println("Введите 1 или 0 для выключения");
-            b = sc.nextInt();
-        } while (b >= 2 || b < 0);
-        if (a == b) {
-            System.out.println("комп выключен");
-        } else {
-            System.out.println("не удалось выключить");
+
+        if (deathResource > 0) {
+            int a = rnd.nextInt(2);
+            int b = sc.nextInt();
+            do {
+                System.out.println("Введите 1 или 0 для выключения");
+                b = sc.nextInt();
+            } while (b >= 2 || b < 0);
+            if (a == b) {
+                System.out.println("комп выключен");
+                deathResource--;
+            } else {
+                System.out.println("не удалось выключить");
+                off();
+            }
         }
     }
 }
